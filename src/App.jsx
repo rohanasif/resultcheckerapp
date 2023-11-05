81f0293e681523f367c251d25a440bd4ea4edcf5
rohanasif1990 <rohanasif1990@gmail.com>
2023-11-05 22:51:17 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+export default App;
