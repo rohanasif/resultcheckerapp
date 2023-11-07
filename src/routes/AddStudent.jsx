3870a210422511e2316c191c4476ca2261fa5f72
rohanasif1990 <rohanasif1990@gmail.com>
2023-11-07 11:31:11 +0500

    submitting student handled

--- src/routes/AddStudent.jsx
+++ src/routes/AddStudent.jsx
@@ -1,23 +1,31 @@
 import { Form, Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
-import { addStudent } from "../actions";
+import { addStudent, getStudents, selectStudent } from "../actions";
 import { useNavigate } from "react-router-dom";
 const AddStudent = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [name, setName] = useState("");
   const [rollNo, setRollNo] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const students = await dispatch(getStudents());
+    console.log(students);
+    const isRegistered = students.find((student) => rollNo === student.rollNo);
+    if (isRegistered) {
+      const student = await dispatch(selectStudent(rollNo));
+      const marks = student.marks;
+    } else {
+      dispatch(addStudent({ name, rollNo }));
+    }
+    navigate(`/submit/${rollNo}`);
+  };
   return (
     <div>
       <h1>Enter Student Details</h1>
-      <Form
-        onSubmit={(e) => {
-          e.preventDefault();
-          dispatch(addStudent({ name, rollNo }));
-          navigate(`/submit/${rollNo}`);
-        }}
-      >
+      <Form onSubmit={(e) => handleSubmit(e)}>
         <Form.Group>
           <Form.Label>Full Name</Form.Label>
           <Form.Control
